342f963a97c85f64f0b51c4ae1d88bfc6b37b6e7
Planke85 <coa85@outlook.com>
2021-03-25 14:35:47 +0100

    Updated

---
+++ Class02/CashMachine/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CashMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cash Machine");
+
+            int bankCash = 150;
+
+            Console.WriteLine("How much money you want to reise?");
+            string cashInput = Console.ReadLine();
+
+            bool convertCashInput = int.TryParse(cashInput, out int cash);
+
+            if (convertCashInput)
+            {
+
+                if(cash < 1)
+                {
+                    Console.WriteLine("Cash value must be greater than zero!");
+                }
+                else
+                {
+
+                    if(bankCash >= cash)
+                    {
+                        int resultCash = bankCash - cash;
+                        Console.WriteLine($"On your account you have {resultCash}$");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You don't have that much money, try with a smaller amount!");
+                    }
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Wrong input, please try again!");
+            }
+        }
+    }
+}
