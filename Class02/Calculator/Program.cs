3a4b391136f513876b700dd9863360b2f3ee07d7
Planke85 <coa85@outlook.com>
2021-03-25 13:38:38 +0100

    Class02 Homework

--- Class02/Calculator/Program.cs
+++ Class02/Calculator/Program.cs
@@ -14,36 +14,30 @@ namespace Calculator
             Console.WriteLine("Please enter second number:");
             string secondNumber = Console.ReadLine();
 
-            Console.WriteLine("Please enter Operation:");
-            Console.WriteLine("Press 1 for Addition");
-            Console.WriteLine("Press 2 for Subtraction");
-            Console.WriteLine("Press 3 for Multiplication");
-            Console.WriteLine("Press 4 for Division");
-            Console.WriteLine("Press 5 for Modulus");
-            string calculatorOperation = Console.ReadLine();
+            Console.WriteLine("Please enter Operation ( + , - , * , / , % ):");
+            string operation = Console.ReadLine();
 
             bool successConvertNum1 = int.TryParse(firstNumber, out int num1);
             bool successConvertNum2 = int.TryParse(secondNumber, out int num2);
-            bool successConvertOperation = int.TryParse(calculatorOperation, out int operation);
 
-            if (successConvertNum1 && successConvertNum2 && successConvertOperation)
+            if (successConvertNum1 && successConvertNum2)
             { 
                 
                 switch (operation)
                 {
-                    case 1:
+                    case "+":
                         Console.WriteLine($"Result {num1} + {num2} = {num1 + num2}");
                         break;
-                    case 2:
+                    case "-":
                         Console.WriteLine($"Result {num1} - {num2} = {num1 - num2}");
                         break;
-                    case 3:
+                    case "*":
                         Console.WriteLine($"Result {num1} * {num2} = {num1 * num2}");
                         break;
-                    case 4:
+                    case "/":
                         Console.WriteLine($"Result {num1} / {num2} = {num1 / num2}");
                         break;
-                    case 5:
+                    case "%":
                         Console.WriteLine($"Result {num1} % {num2} = {num1 % num2}");
                         break;
                     default:
