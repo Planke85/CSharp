1a3f6a2c8f65f5600165b25cc1778a3d90d18466
Planke85 <coa85@outlook.com>
2021-03-25 09:34:06 +0100

    First Commit

---
+++ Class01/BonusHomework/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BonusHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("     *   ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("    ***   ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("   *****   ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("  *******   ");
+            Console.Beep(500, 500);
+
+            Console.WriteLine("---------------------");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("   *   ");
+            Console.WriteLine("   *   ");
+            Console.WriteLine("  ***   ");
+            Console.WriteLine(" * * *   ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("   *   ");
+            Console.WriteLine("   *   ");
+            Console.WriteLine("  * *   ");
+            Console.WriteLine(" *   *   ");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Class01/Homework/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("    *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("  *****");
+            Console.WriteLine(" *******");
+            Console.WriteLine("*********");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("    *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("  * * *");
+            Console.WriteLine("    *");
+            Console.WriteLine("   * *");
+            Console.WriteLine("  *   *");
+
+            Console.ReadLine();
+        }
+    }
+}
