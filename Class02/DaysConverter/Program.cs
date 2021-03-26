6de3f4c0596129eeff82ba1981a8ab2d5dfda102
Planke85 <coa85@outlook.com>
2021-03-26 15:40:13 +0100

    Bonus Homework

---
+++ Class02/DaysConverter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DaysConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number of days:");
+            string numberOfDays = Console.ReadLine();
+
+            bool convertNumOfDays = int.TryParse(numberOfDays, out int daysNumber);
+
+            if (convertNumOfDays)
+            {
+                int year = daysNumber / 365;
+                int day = daysNumber % 365;
+                int weeks = day / 7;
+                day = day % 7;
+
+                if(year == weeks && year == day)
+                {
+                    Console.WriteLine("You've entered a special number of days!");
+                }
+                else
+                {
+                    Console.WriteLine($"Years: {year}, Weeks: {weeks}, Days: {day}");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Wrong input, try again!");
+            }
+
+        }
+    }
+}
---
+++ Class02/LargestNumber/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter first number:");
+            string firstNumber = Console.ReadLine();
+
+            Console.WriteLine("Please enter second number:");
+            string secondNumber = Console.ReadLine();
+
+            Console.WriteLine("Please enter third number:");
+            string thirdNumber = Console.ReadLine();
+
+            Console.WriteLine("Please enter fourth number:");
+            string fourthNumber = Console.ReadLine();
+
+            bool convertNum1 = int.TryParse(firstNumber, out int num1);
+            bool convertNum2 = int.TryParse(secondNumber, out int num2);
+            bool convertNum3 = int.TryParse(thirdNumber, out int num3);
+            bool convertNum4 = int.TryParse(fourthNumber, out int num4);
+
+            if(convertNum1 && convertNum2 && convertNum3 && convertNum4)
+            {
+
+                if(num1 >= num2 && num1 >= num3 && num1 >= num4)
+                {
+                    Console.WriteLine($"The largest of {num1}, {num2}, {num3} and {num4} is: {num1}");
+                }
+                else if(num2 >= num1 && num2 >= num3 && num2 >= num4)
+                {
+                    Console.WriteLine($"The largest of {num1}, {num2}, {num3} and {num4} is: {num2}");
+                }
+                else if (num3 >= num1 && num3 >= num2 && num3 >= num4)
+                {
+                    Console.WriteLine($"The largest of {num1}, {num2}, {num3} and {num4} is: {num3}");
+                }
+                else
+                {
+                    Console.WriteLine($"The largest of {num1}, {num2}, {num3} and {num4} is: {num4}");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Wrong input, try again!");
+            }
+
+        }
+    }
+}
